cec47042784258a5826632470e294c22d558e485
siweiwang <wangsiwei2003@hotmail.com>
2016-09-23 00:21:01 -0400

    add create and remove user endpoint

--- web_server.go
+++ web_server.go
@@ -23,11 +23,11 @@ func main() {
         fmt.Fprint(w, "live!\n")
     })
 
-    // Handle resource user by id
+    // Get resource user by id
     r.GET("/user/:id", func (w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
         // Simply write some test data for now
-        new_user := models.User{
+        found_user := models.User{
             Name:   "John Doe",
             Gender: "male",
             Age:    34,
@@ -35,12 +35,39 @@ func main() {
         }
 
         // Marshal provided interface into JSON structure
-        uj, _ := json.Marshal(new_user)
+        uj, _ := json.Marshal(found_user)
 
         // Write content-type, stautscode and payload
         w.Header().Set("Content-Type", "application/json")
         w.WriteHeader(200)
         fmt.Fprintf(w, "%s", uj)
+    })
+
+    // Creates resource user by id
+    r.POST("/user", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {  
+        // Stub an user to be populated from the body
+        new_user := models.User{}
+
+        // Populate the user data
+        json.NewDecoder(r.Body).Decode(&new_user)
+
+        // Add an Id
+        new_user.Id = "foo"
+
+        // Marshal provided interface into JSON structure
+        uj, _ := json.Marshal(new_user)
+
+        // Write content-type, statuscode, payload
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(201)
+        fmt.Fprintf(w, "%s", uj)
+    })
+
+    // Removes resource user by id
+    r.DELETE("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {  
+
+        // TODO: only write status for now
+        w.WriteHeader(200)
     })
 
     // Fire up the server
